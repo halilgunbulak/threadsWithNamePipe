c66d17e06c5e5afc9913387b17774d32ae042639
Halil İbrahim GÜNBULAK <110221633+halilgunbulak@users.noreply.github.com>
2023-01-02 20:31:32 +0300

    reveive.c and write.c worked with create , delete and write file process

--- read.c
+++ read.c
@@ -8,6 +8,11 @@
 
 #define PIPE_NAME "my_named_pipe"
 #define PIPE_NAME2 "my_named_pipe2"
+#define PIPE_NAME_CREATE "my_named_create"
+#define PIPE_NAME_DELETE "my_named_delete"
+#define PIPE_NAME_READ "my_named_read"
+#define PIPE_NAME_WRITE "my_named_write"
+#define PIPE_NAME_EXIT "my_named_exit"
 
 #define BUFFER_SIZE 1024
 //char array size return;
@@ -17,82 +22,99 @@ void remove_newline_char(char *str);
 void splitP(char *str, char **parsed, char *delim);
 //char elemanı boş mu değil mi buna göre return vermekte boş ise 1 dolu ise 0 dönmektedir.
 int charIsEmpty(char *str);
+//eger char degeri içeriyorsa 1 dönüyor.
 
 
 pthread_mutex_t mutex;
 int fd;
 int fd2;
+int fd_create;
+int fd_delete;
+int fd_write;
+int fd_read;
+int fd_exit;
  char* buffer ;
+ char* buffer_create ;
+ char* buffer_delete ;
+ char* buffer_read ;
+ char* buffer_write ;
+ char* buffer_exit ;
+
 void *create_file(void *arg)
 {   
   printf("create file komutu dinlemeye başladi\n");
    // Read from the named pipe and print the data to the console
   while (1) {
-    
-    ssize_t bytes_read = read(fd, buffer, BUFFER_SIZE);
+    memset(buffer_create, 0, sizeof(buffer_create));
+    ssize_t bytes_read_create = read(fd_create, buffer_create, BUFFER_SIZE);
       pthread_mutex_lock(&mutex); 
        
     
-    if (bytes_read < 0) {
+    if (bytes_read_create < 0) {
       perror("read");
       break;
     }
-    if (bytes_read == 0) {
+    if (bytes_read_create == 0) {
       break;
     }
     char command[BUFFER_SIZE];
     //printf("buffer: %s",buffer);
    char *splitBuffer[200] = {NULL};
         
-    splitP(buffer,splitBuffer," ");
-    sscanf(buffer, "%s", command);
+    splitP(buffer_create,splitBuffer," ");
+    sscanf(buffer_create, "%s", command);
     //printf("buffer: %s %d",buffer,bytes_read);
 
-
+     printf("buraya geldi");
     char *fileName="yeni.txt";
      if(strcmp(command,"create")==0){
        
        int i=0;
        while(splitBuffer[i]!=NULL){
          // printf("create command %s \n",splitBuffer[i]);
-          i++;
-          if(i==1){
+        
+          if(strstr(splitBuffer[i],".txt")!=NULL){
             fileName=splitBuffer[i];
           }
+          i++;
+          
        }
 
        printf("filename: %s", fileName);
        FILE *fp;
        fp = fopen(fileName, "a+");
        fclose(fp);
+   
+       
       
-       sprintf(buffer, "%s adli file_olusturuldu", fileName);
 
        // cevap gönderme başlangıç
-       printf("buray akadar geldi\n");
-       int bufferSize = size_char(buffer);
-       printf("buffer : %s  buffer boyu %d \n", buffer, bufferSize);
+       
+       int bufferSize = size_char(buffer_create);
+       printf("buffer : %s  buffer boyu %d \n", buffer_create, bufferSize);
        //fwrite(buffer, 1, bufferSize, stdout);
-        fd2 = open(PIPE_NAME2, O_WRONLY);
-          if (fd2 < 0) {
-            perror("open");
-            return 1;
-          } 
-       ssize_t bytes_written = write(fd2, buffer, bufferSize);
-       if (bytes_written < 0)
+      
+       ssize_t bytes_written_create = write(fd2, buffer_create, bufferSize);
+       if (bytes_written_create < 0)
        {
           perror("write");
           break;
        } // cevap gönderme bitiş
-       close(fd2);
 
+       close(fd2);
+       fd2 = open(PIPE_NAME2, O_WRONLY);
+          if (fd2 < 0) {
+            perror("open");
+            return 1;
+          } 
        
 
      }
-
+     printf("create cikis yapti\n");
      pthread_mutex_unlock(&mutex);
   }
-
+  close(fd_create);
+ 
   // dosya oluşturma işlemini kilitleyin
     printf("create file\n");
     // dosya oluşturma işlemleri burada gerçekleştirin
@@ -100,24 +122,195 @@ void *create_file(void *arg)
    // dosya oluşturma işlemini kilidi açın
 }
 
-void *delete_file(void *arg)
-{
+void *delete_file(void *arg){
     printf("delete file komutu dinlemeye başladi\n");
+  while (1) {
+    
+    ssize_t bytes_read_delete = read(fd_delete, buffer_delete, BUFFER_SIZE);
+      pthread_mutex_lock(&mutex); 
+       
+    
+    if (bytes_read_delete < 0) {
+      perror("read");
+      break;
+    }
+    if (bytes_read_delete == 0) {
+      break;
+    }
+    char command[BUFFER_SIZE];
+    //printf("buffer: %s",buffer);
+   char *splitBuffer[200] = {NULL};
+        
+    splitP(buffer_delete,splitBuffer," ");
+    sscanf(buffer_delete, "%s", command);
+    //printf("buffer: %s %d",buffer,bytes_read);
 
+    
+    char *fileName="yeni.txt";
+    char *fileNameTemp="yeni.txt";
+     if(strcmp(command,"delete")==0){
+       
+       int i=0;
+       while(splitBuffer[i]!=NULL){
+         // printf("create command %s \n",splitBuffer[i]);
+         
+          if(i==1){
+            printf("splitBuffer i: %s \n",splitBuffer[i]);
+            fileName=splitBuffer[i];
+            fileNameTemp=splitBuffer[i];
+          }
+           i++;
+       }
+       char *temp;
+      // printf("filename: %s\n", fileName);
+      //  printf("filenameTemp: %s\n", fileNameTemp);
+       if (remove(fileName) ==0) {
+        
+          printf("filenameTemp: %s\n", fileNameTemp);
+         
+          
+         //sprintf(buffer, "%s adli dosya basariyla silindi", fileName);
+        
+       } else {
+       
+        
+          
+       }
+
+       remove_newline_char(buffer_delete);
+       // cevap gönderme başlangıç
+      
+      
+      
+       printf("buffer : %s  buffer boyu %d \n", buffer_delete, size_char(buffer_delete));
+       //fwrite(buffer, 1, bufferSize, stdout);
+      
+       ssize_t bytes_written_delete = write(fd2, buffer_delete, size_char(buffer_delete));
+       if (bytes_written_delete < 0)
+       {
+          perror("write");
+          break;
+       } // cevap gönderme bitiş
+       close(fd2);
+     
+       fd2 = open(PIPE_NAME2, O_WRONLY);
+          if (fd2 < 0) {
+            perror("open");
+            return 1;
+          } 
+       
+
+     }
+     printf("delete cikis yapti\n");
+    
+     pthread_mutex_unlock(&mutex);
+  }
+  close(fd_delete);
 
   // dosya oluşturma işlemini kilitleyin
-    printf("delete file\n");
+    printf("delete file\n\n");
    
 }
 
 void *write_file(void *arg)
 {
-    pthread_mutex_lock(&mutex); // dosya yazma işlemini kilitleyin
- printf("write file\n");
-    // dosya yazma işlemleri burada gerçekleştirin
+   printf("write file komutu dinlemeye başladi\n");
+  while (1) {
     
+    ssize_t bytes_read_write = read(fd_write, buffer_write, BUFFER_SIZE);
+      pthread_mutex_lock(&mutex); // dosya yazma işlemini kilitle
+       remove_newline_char(buffer);
+       printf("buffer ilk hal %s",buffer);
+    
+    if (bytes_read_write < 0) {
+      perror("read");
+      break;
+    }
+    if (bytes_read_write == 0) {
+      break;
+    }
+    char command[BUFFER_SIZE];
+    //printf("buffer: %s",buffer);
+   char *splitBuffer[200] = {NULL};
+        
+    splitP(buffer_write,splitBuffer," ");
+    sscanf(buffer_write, "%s", command);
+    //printf("buffer: %s %d",buffer,bytes_read);
 
-    pthread_mutex_unlock(&mutex); // dosya yazma işlemini kilidi açın
+    
+    char *fileName="yeni.txt";
+    char *fileNameTemp="yeni.txt";
+     if(strcmp(command,"write")==0){
+       
+       int i=0;
+  
+       int satir=0;
+       while(splitBuffer[i]!=NULL){
+         // printf("create command %s \n",splitBuffer[i]);
+        
+          if(strstr(splitBuffer[i],".txt")!=NULL){
+            fileName=splitBuffer[i];
+            satir=i+1;
+          }
+          i++;
+          
+       }
+       printf("filename %s /n",fileName);
+       FILE *fptr;
+       fptr=fopen(fileName,"a+");
+      if(fptr == NULL)
+      {
+          printf("Error!");   
+                       
+      }else{
+        for(int j=satir;splitBuffer[j] !=NULL ;j++){
+          fputs(splitBuffer[j], fptr);
+          fputs(" ", fptr);
+        }
+         fputs("\n", fptr);
+      }
+        fclose(fptr);
+
+      
+       char *temp;
+      // printf("filename: %s\n", fileName);
+      //  printf("filenameTemp: %s\n", fileNameTemp);
+   
+
+       remove_newline_char(buffer_write);
+       // cevap gönderme başlangıç
+      
+      
+      
+       printf("buffer : %s  buffer boyu %d \n", buffer_write, size_char(buffer_write));
+       //fwrite(buffer, 1, bufferSize, stdout);
+      
+       ssize_t bytes_written_write = write(fd2, buffer_write, size_char(buffer_write));
+       if (bytes_written_write < 0)
+       {
+          perror("write");
+          break;
+       } // cevap gönderme bitiş
+       close(fd2);
+     
+       fd2 = open(PIPE_NAME2, O_WRONLY);
+          if (fd2 < 0) {
+            perror("open");
+            return 1;
+          } 
+       
+
+     }
+     printf("write cikis yapti\n");
+    
+     pthread_mutex_unlock(&mutex);// dosya yazma işlemini kilidini ac
+  }
+  strcpy(buffer_write,"");
+  close(fd_delete);
+
+  
+    printf("write file methodu bitti\n\n");
+   
 }
 
 void *exit_program(void *arg)
@@ -143,15 +336,56 @@ int main(int argc, char** argv) {
 
   
     buffer = malloc(BUFFER_SIZE);
+      buffer_read = malloc(BUFFER_SIZE);
+        buffer_create = malloc(BUFFER_SIZE);
+          buffer_write = malloc(BUFFER_SIZE);
+            buffer_exit = malloc(BUFFER_SIZE);
+              buffer_delete = malloc(BUFFER_SIZE);
 
 
     char * myfifo = PIPE_NAME;
     char * myfifo2 = PIPE_NAME2;
+    char * myfifocreate =PIPE_NAME_CREATE;
+     char * myfifodelete =PIPE_NAME_DELETE;
+     char * myfifowrite =PIPE_NAME_WRITE;
+     char * myfiforead =PIPE_NAME_READ;
+     char * myfifoexit =PIPE_NAME_EXIT;
     mkfifo(myfifo, 0666);
     mkfifo(myfifo2, 0666);
+    mkfifo(myfifocreate, 0666);
+    mkfifo(myfifodelete, 0666);
+    mkfifo(myfifowrite, 0666);
+    mkfifo(myfiforead, 0666);
+    mkfifo(myfifocreate, 0666);
+  
   // Open the named pipe for reading
   fd = open(PIPE_NAME, O_RDONLY);
   if (fd < 0) {
+    perror("open");
+    return 1;
+  }
+    fd_create= open(PIPE_NAME_CREATE, O_RDONLY);
+  if (fd_create < 0) {
+    perror("open");
+    return 1;
+  }
+    fd_delete= open(PIPE_NAME_DELETE, O_RDONLY);
+  if (fd_delete < 0) {
+    perror("open");
+    return 1;
+  }
+    fd_write= open(PIPE_NAME_WRITE, O_RDONLY);
+  if (fd_write < 0) {
+    perror("open");
+    return 1;
+  }
+    fd_read= open(PIPE_NAME_READ, O_RDONLY);
+  if (fd_read < 0) {
+    perror("open");
+    return 1;
+  }
+    fd_exit= open(PIPE_NAME_EXIT, O_RDONLY);
+  if (fd_exit < 0) {
     perror("open");
     return 1;
   }
@@ -163,6 +397,26 @@ int main(int argc, char** argv) {
   // Allocate a buffer for reading from the named pipe
  
   if (buffer == NULL) {
+    perror("malloc");
+    return 1;
+  }
+  if (buffer_create == NULL) {
+    perror("malloc");
+    return 1;
+  }
+  if (buffer_delete == NULL) {
+    perror("malloc");
+    return 1;
+  }
+  if (buffer_exit == NULL) {
+    perror("malloc");
+    return 1;
+  }
+  if (buffer_write == NULL) {
+    perror("malloc");
+    return 1;
+  }
+  if (buffer_read == NULL) {
     perror("malloc");
     return 1;
   }
@@ -183,6 +437,11 @@ int main(int argc, char** argv) {
     pthread_mutex_destroy(&mutex);
   // Clean up resources
   free(buffer);
+  free(buffer_write);
+  free(buffer_read);
+  free(buffer_delete);
+  free(buffer_exit);
+  free(buffer_create);
   close(fd);
   close(fd2);
   printf("read main bitti");
@@ -235,3 +494,4 @@ void remove_newline_char(char *str)
         str++; // increse str pointer
     }
 }
+
--- write.c
+++ write.c
@@ -6,12 +6,24 @@
 
 #define PIPE_NAME "my_named_pipe"
 #define PIPE_NAME2 "my_named_pipe2"
+#define PIPE_NAME_CREATE "my_named_create"
+#define PIPE_NAME_DELETE "my_named_delete"
 #define BUFFER_SIZE 1024
 void remove_newline_char(char *str);
 int main(int argc, char** argv) {
   // Open the named pipe for writing
   int fd = open(PIPE_NAME, O_WRONLY);
   if (fd < 0) {
+    perror("open");
+    return 1;
+  }
+    int fd_create= open(PIPE_NAME_CREATE, O_WRONLY);
+  if (fd_create < 0) {
+    perror("open");
+    return 1;
+  }
+    int fd_delete = open(PIPE_NAME_DELETE, O_WRONLY);
+  if (fd_delete < 0) {
     perror("open");
     return 1;
   }
@@ -32,33 +44,41 @@ int main(int argc, char** argv) {
     
   // Read data from stdin and write it to the named pipe
   while (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
-    ssize_t bytes_written = write(fd, buffer, strlen(buffer));
+    
+    
+    ssize_t bytes_written = write(fd_create, buffer, strlen(buffer));
     sscanf(buffer, "%s", command);
-    if (bytes_written < 0 || strcmp(command,"exit")==0) {
-      perror("write");
-      break;
+    int commandId=commandToInt(command);
+    if(commandId==1){
+               printf("Command: %s %d \n",command,commandId);
+              if (bytes_written < 0 || strcmp(command,"exit")==0) {
+                perror("write");
+                break;
+              }
+              ssize_t bytes_read = read(fd2, buffer, BUFFER_SIZE); 
+              remove_newline_char(buffer);
+              printf("buffer: %s \n",buffer);
+              if (bytes_read < 0) {
+                perror("read");
+                break;
+              }
+              if (bytes_read == 0) {
+                break;
+              }
+              char command[BUFFER_SIZE];
+              sscanf(buffer, "%s", command);
+              if(strcmp(command,"exit")==0){
+                  printf("Proggress exit : worked exit command");
+                  break;
+              }
     }
-    ssize_t bytes_read = read(fd2, buffer, BUFFER_SIZE);
-    remove_newline_char(buffer);
-    printf("buffer: %s \n",buffer);
-    if (bytes_read < 0) {
-      perror("read");
-      break;
-    }
-    if (bytes_read == 0) {
-      break;
-    }
-    char command[BUFFER_SIZE];
-     sscanf(buffer, "%s", command);
-    if(strcmp(command,"exit")==0){
-        printf("Proggress exit : worked exit command");
-        break;
-    }
+    
   }
 
   // Clean up resources
   free(buffer);
   close(fd);
+   close(fd_create);
   return 0;
 }
 
@@ -73,4 +93,18 @@ void remove_newline_char(char *str)
         }
         str++; // increse str pointer
     }
+}
+
+int commandToInt(char* eleman){
+   if(strcmp(eleman,"create")==0){
+    return 1;
+   }else if(strcmp(eleman,"delete")==0){
+    return 2;
+   }else if(strcmp(eleman,"write")==0){
+    return 3;
+   }else if(strcmp(eleman,"read")==0){
+    return 4;
+   }else if(strcmp(eleman,"exit")==0){
+    return 5;
+   }else return 0;
 }
